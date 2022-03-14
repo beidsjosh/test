24b7a4ff10fbfd2c810c036e4c24ba26f88114d8
beidsjosh <joshua.beidham@griffithuni.edu.au>
2022-03-15 09:05:52 +1000

    Initial Commit

---
+++ helloGUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloGUI
+//
+//  Created by Joshua Beidham on 15/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Josh!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ helloGUI/helloGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloGUIApp.swift
+//  helloGUI
+//
+//  Created by Joshua Beidham on 15/3/22.
+//
+
+import SwiftUI
+
+@main
+struct helloGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
